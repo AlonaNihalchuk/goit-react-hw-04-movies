034d8268af70768fc39178bd3964639aba157b11
AlonaNihalchuk <acetnikopol@gmail.com>
2021-08-13 12:54:38 +0200

    Route

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="/" />
+    </div>
+  );
+}
+
+export default App;
+// facd26345a3f4f9845cad78add290316
